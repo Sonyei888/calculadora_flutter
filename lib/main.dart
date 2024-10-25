7c50ba009387b51e24b022f4188a9e21dfca4dc3
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-25 17:54:21 -0500

    Diseño Inicial - Primer container

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const CalcApp());
@@ -20,11 +21,38 @@ class _CalcAppState extends State<CalcApp> {
       home: Scaffold(
         backgroundColor: Color(0xFF283637),
         body: Container(
-          padding: EdgeInsets.all(12),
+          padding: const EdgeInsets.all(12),
           child: Column(
-            
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                alignment: const Alignment(1.0, 1.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text(
+                    "Historial",
+                    style: GoogleFonts.rubik(
+                        textStyle: const TextStyle(
+                            fontSize: 24, color: Color(0xFF545F61))),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: const Alignment(1.0, 1.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: (Text(
+                    "+",
+                    style: GoogleFonts.rubik(
+                        textStyle:
+                            const TextStyle(fontSize: 48, color: Colors.white)),
+                  )),
+                ),
+              )
+            ],
           ),
         ),
+        
       ),
     );
   }
