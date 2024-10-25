89664517d8f21d6f5c3f1466a2d4f17fd736ed72
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-25 17:30:48 -0500

    Caga inicial del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CalcApp());
+}
+
+class CalcApp extends StatefulWidget {
+  const CalcApp({super.key});
+
+  @override
+  State<CalcApp> createState() => _CalcAppState();
+}
+
+class _CalcAppState extends State<CalcApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
